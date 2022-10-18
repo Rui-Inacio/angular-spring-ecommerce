0e4307d3a1d98be7824fe4e14f0346beead854fc
Rui Inácio <rmy_98@hotmail.com>
2022-10-18 04:28:03 +0100

    FullStack Angular SpringBoot

---
+++ backend/spring-boot-ecommerce/src/main/java/com/kraken/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.kraken.ecommerce.dao;
+
+import com.kraken.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
